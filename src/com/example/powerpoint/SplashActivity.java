baacfa547d1b6a92f3123e21dc23cd090f9e36c0
Parnit Sainion <parnits@gmail.com>
2013-04-30 22:26:10 -0700

    Fixed Changes

---
+++ src/com/example/powerpoint/SplashActivity.java
@@ -0,0 +1,53 @@
+package com.example.powerpoint;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SplashActivity extends Activity 
+{
+	protected int waitTime  = 2000;
+	
+	public void onCreate( Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.app_splash);
+		
+		ImageView splashImageView = (ImageView) findViewById(R.id.appSpashImageView);
+		
+		int splashId;
+		
+		splashId= R.drawable.ic_launcher;
+		
+		Bitmap splashBitmap = BitmapFactory.decodeResource(getResources(), splashId);
+		
+		splashImageView.setImageBitmap(splashBitmap);
+		
+		final SplashActivity thisSplash = this;
+		
+		Thread splashThread = new Thread(){
+			
+			public void run ()
+			{
+				try{
+					synchronized(this){
+						wait(waitTime);
+					}
+				}catch(Exception e)
+				{
+					
+				}
+				
+				Intent intent= new Intent();
+				intent.setClass(thisSplash, MainActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		};
+		splashThread.start();
+	}
+}
